be74543754007cc175e7d574a5e2ad8a6b37352b
Nooheat <nooheat1228@gmail.com>
2018-11-25 18:12:35 +0900

    C3.4.3.4 요구사항 4 - 302 status code 적용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -41,7 +41,7 @@ public class RequestHandler extends Thread {
                 User user = new User(request.getParam("userId"), request.getParam("password"),
                         request.getParam("name"), request.getParam("email"));
                 DataBase.addUser(user);
-                response200(dos, user.toString().getBytes());
+                response302(dos, "/index.html");
                 return;
             }
 
@@ -63,6 +63,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302(DataOutputStream dos, String location) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + location + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
